2ca12bb1674fd25767399e1102e9b36be6b6225d
nikitrifonov <nikitrifonov@gmail.com>
2018-02-08 15:36:34 +0200

    #9 - Added DeveloperConfig.cs and the keys in the app settings

--- NikiCars/NikiCars.Console/Program.cs
+++ NikiCars/NikiCars.Console/Program.cs
@@ -40,23 +40,23 @@ namespace NikiCars.Console
             //var client = new CommandClient(DependencyContainer.Resolve<Invoker>());
             //var result = client.SendRequest(command, user);
 
-            UserRole role;
-            using (var repo = DependencyContainer.Resolve<IRepository<UserRole>>())
-            {
-                role = repo.GetByID(1);
-            }
+            //UserRole role;
+            //using (var repo = DependencyContainer.Resolve<IRepository<UserRole>>())
+            //{
+            //    role = repo.GetByID(1);
+            //}
 
-            using (var repo = DependencyContainer.Resolve<IUserRepository>())
-            {
-                var user = repo.GetUserByName("Gogoto", new List<Data.Includes.UserIncludes> { Data.Includes.UserIncludes.UserRoles });
+            //using (var repo = DependencyContainer.Resolve<IUserRepository>())
+            //{
+            //    var user = repo.GetUserByName("Gogoto", new List<Data.Includes.UserIncludes> { Data.Includes.UserIncludes.UserRoles });
 
-                user.Roles.Remove(role);
-                user.Email = "nik@gnam.xom";
+            //    user.Roles.Remove(role);
+            //    user.Email = "nik@gnam.xom";
 
-                var res = repo.Update(user);
+            //    var res = repo.Update(user);
 
-                var resToCompare = repo.GetUserByName("Gogoto", new List<Data.Includes.UserIncludes> { Data.Includes.UserIncludes.UserRoles });
-            }
+            //    var resToCompare = repo.GetUserByName("Gogoto", new List<Data.Includes.UserIncludes> { Data.Includes.UserIncludes.UserRoles });
+            //}
 
             //User usera = new User();
             //usera.Address = "Odrin";
---
+++ NikiCars/NikiCars.Console/Utility/DeveloperConfig.cs
@@ -0,0 +1,58 @@
+﻿using System.Configuration;
+using NikiCars.Command.Interfaces;
+
+namespace NikiCars.Console.Utility
+{
+    public class DeveloperConfig : IConfig
+    {
+        private static string signingKey;
+        private static string secutityKey;
+        private static string connectionString;
+
+        static DeveloperConfig()
+        {
+            signingKey = GetSigningKey();
+            secutityKey = GetSecutityKey();
+            connectionString = GetConnectionString();
+        }
+
+        public string SigningKey
+        {
+            get
+            {
+                return signingKey;
+            }
+        }
+
+        public string SecutityKey
+        {
+            get
+            {
+                return secutityKey;
+            }
+        }
+
+        public string ConnectionString
+        {
+            get
+            {
+                return connectionString;
+            }
+        }
+
+        private static string GetSigningKey()
+        {
+            return ConfigurationManager.AppSettings["signingKey"];
+        }
+
+        private static string GetSecutityKey()
+        {
+            return ConfigurationManager.AppSettings["secutityKey"];
+        }
+
+        private static string GetConnectionString()
+        {
+            return ConfigurationManager.AppSettings["ConnectionString"];
+        }
+    }
+}
