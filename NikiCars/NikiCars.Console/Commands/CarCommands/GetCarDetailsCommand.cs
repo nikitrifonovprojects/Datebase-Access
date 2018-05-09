2f536aa9de858b2e63aae7ea0ceae39659d74c82
nikitrifonovprojects <do_not_stop@abv.bg>
2018-05-09 15:54:27 +0300

    #33 - added a command for car details

---
+++ NikiCars/NikiCars.Console/Commands/CarCommands/GetCarDetailsCommand.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using NikiCars.Command.Framework;
+using NikiCars.Command.Framework.Attributes;
+using NikiCars.Command.Framework.Routing;
+using NikiCars.Command.Interfaces;
+using NikiCars.Command.Validation;
+using NikiCars.Data.Models;
+using NikiCars.Data.Models.Includes;
+using NikiCars.Models.CarModels;
+using NikiCars.Services.Interfaces;
+using NikiCars.Services.Mapping;
+
+namespace NikiCars.Console.Commands.CarCommands
+{
+    [Validate]
+    [CommandRoute("get CarDetails")]
+    public class GetCarDetailsCommand : BaseCommand<ShowCarDetailsModel>
+    {
+        private ICarService service;
+        private IMappingService mapping;
+
+        public GetCarDetailsCommand(CommandContext context, ICarService service, IValidator validation, IMappingService mapping) 
+            : base(context, validation)
+        {
+            this.service = service;
+            this.mapping = mapping;
+        }
+
+        protected override ICommandResult ExecuteAction(ShowCarDetailsModel item)
+        {
+            var includes = new List<CarIncludes>() { CarIncludes.Colour, CarIncludes.Extras, CarIncludes.FuelType, CarIncludes.GearboxType, CarIncludes.CarMake, CarIncludes.CarModel };
+            Car car = this.service.GetById(item.ID, includes);
+
+            if (car == null)
+            {
+                return this.Error($"Car with ID: {item.ID} not found");
+            }
+
+            var result = this.mapping.Map<AdvancedSearchCarModel>(car);
+
+            return this.Success(result);
+        }
+
+        public override void Dispose()
+        {
+            this.service.Dispose();
+        }
+    }
+}
---
+++ NikiCars/NikiCars.Models/CarModels/ShowCarDetailsModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NikiCars.Models.CarModels
+{
+    public class ShowCarDetailsModel
+    {
+        [Range(1, int.MaxValue)]
+        public int ID { get; set; }
+        
+        public int NumberOfDoorsID { get; set; }
+
+        public int? FuelTypeID { get; set; }
+
+        public int? ColourID { get; set; }
+
+        public int? GearboxTypeID { get; set; }
+
+        public int HorsePower { get; set; }
+
+        public DateTime FirstRegistrationDate { get; set; }
+
+        public int EngineCapacity { get; set; }
+
+        public int Kilometers { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Discription { get; set; }
+
+        public string Title { get; set; }
+    }
+}
